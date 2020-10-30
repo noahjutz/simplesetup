1b6f8105a86ffac369272adbccc6839b804960d1
Noah Jutz <noahjutz@tutanota.de>
2020-10-30 21:41:19 +0100

    Update logging

--- simplesetup.sh
+++ simplesetup.sh
@@ -1,27 +1,36 @@
 #!/bin/sh
 
-# Request sudo
-echo "Requesting root privileges."
-sudo echo "Thank you!"
+function log() {
+  echo
+  echo "==> $1"
+  echo
+}
 
-# Restore dotfiles
+log "request root privileges"
+sudo log "Thank you!"
+
+log "restore dotfiles"
 dotfiles_url=https://github.com/noahjutz/newdotfiles
 git clone --bare $dotfiles_url $HOME/.cfg
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout -f
 
-# Restore gnome backup
+log "restore gnome backup"
 backup_path=$HOME/.cfg_files/gnome_settings_backup
 dconf load / < $backup_path
 
+log "rankmirrors"
 sudo rankmirrors -f
+
+log "update & upgrade"
 sudo pacman -Syyu
 
-# Install packages
+log "install packages"
 sudo pacman -S --noconfirm $(cat to_install.txt)
 
-# Uninstall bloat
+log "uninstall bloat"
 sudo pacman -Rcns --noconfirm $(cat to_remove.txt)
 sudo pacman -R --noconfirm $(pacman -Qdtq)
 
-# Set shell
+log "set shell"
 sudo chsh -s /usr/bin/fish noah
+
