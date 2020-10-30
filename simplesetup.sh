d3929ff8746a3b63f1f51a5f1495a379dff0c7b1
Noah Jutz <noahjutz@tutanota.de>
2020-10-30 21:21:33 +0100

    Initial commit

---
+++ simplesetup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Request sudo
+echo "Requesting root privileges."
+sudo echo "Thank you!"
+
+# Restore dotfiles
+dotfiles_url=https://github.com/noahjutz/newdotfiles
+git clone --bare $dotfiles_url $HOME/.cfg
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout -f
+
+# Restore gnome backup
+backup_path=$HOME/.cfg_files/gnome_settings_backup
+dconf load / < $backup_path
+
+sudo rankmirrors -f
+sudo pacman -Syyu
+
+# Install packages
+sudo pacman -S --noconfirm neovim fish
+
+# Uninstall bloat
+sudo pacman -Rns --noconfirm gedit totem file-roller gnome-calculator gparted kvantum-qt5 gnome-system-log zsh
+
+# Set shell
+sudo chsh -s /usr/bin/fish noah
